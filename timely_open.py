bb0990d34c497988ecb3b562cb93a8a6684f7a0a
Arnav-crypto <64422703+Arnav-crypto@users.noreply.github.com>
2020-12-09 15:55:12 +0530

    Add files via upload

--- timely_open.py
+++ timely_open.py
@@ -6,18 +6,18 @@ from pynput.keyboard import Controller, Key
 print("sucessfully initialized")
 keyboard = Controller()
 now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
 print("everything good till now")
 class1 = '15:31:00'
 
 while True:
+    current_time = now.strftime("%H:%M:%S")
+    time.sleep(1)
     if current_time == class1 or class2 or class3 or class4 or class5:
-        time.sleep(1)
         webbrowser.open(https://meetingsapac27.webex.com/meet/pr1668878079)
         time.sleep(6)
         keyboard.press(Key.enter)
     else:
-        print("its not time yet its just " + current_time)    
+        print("its not time yet its just " + current_time) 
 
 
 
